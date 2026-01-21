296f01563c2e14ae3078d5d1a3f00b0dbda5459c
nsoumbe <nsoumbe237@icloud.com>
2026-01-21 17:10:35 +0000

    Phase 1 - Architecture Frontend / Backend / DB avec Docker

--- app-backend/main.py
+++ app-backend/main.py
@@ -1,9 +1,10 @@
+
 import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 # Variable d'environnement pour le versionnement (CI/CD)
-VERSION = os.getenv("APP_VERSION", "1.0.0") 
+VERSION = os.getenv("APP_VERSION", "1.0.0")
 
 @app.route('/api/data')
 def get_data():
@@ -11,3 +12,4 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
